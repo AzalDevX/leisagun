802bb0de9fed565937b823a1b007c6d5dfe1a04d
ik_2dm3 <ik_2dm3@H015110.NET>
2023-10-03 08:51:08 +0200

    Initial commit

---
+++ app/src/main/java/com/txurdinaga/rednit_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.txurdinaga.rednit_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
