30ceb150dd49ac0cde98a102614186ae036d9edd
☁ <78152427+ITSXNOOBX@users.noreply.github.com>
2023-10-10 12:52:17 +0200

    I tried to do my best to not blow my head :D
    Added logout button and some kind of navigation menu. :D

--- app/src/main/java/com/txurdinaga/rednit_app/views/HomeActivity.kt
+++ app/src/main/java/com/txurdinaga/rednit_app/views/HomeActivity.kt
@@ -1,16 +1,16 @@
 package com.txurdinaga.rednit_app.views
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import androidx.appcompat.app.*
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import android.widget.*
+import androidx.appcompat.widget.AppCompatImageView
 import com.txurdinaga.rednit_app.R
 import com.txurdinaga.rednit_app.classes.Globals
 
 class HomeActivity : AppCompatActivity() {
-
-//    val globals = application as Globals
 
     private lateinit var card_title : TextView
     private lateinit var card_subtitle : TextView
@@ -24,29 +24,32 @@ class HomeActivity : AppCompatActivity() {
         Log.e("home", "Home activity has started!")
 
         val globals = application as Globals
-//        Toast.makeText(this, globals.current_user?.email.toString(), Toast.LENGTH_SHORT).show()
-
-        // Infla la vista que contiene la "carta"
-        val customCardView = layoutInflater.inflate(R.layout.custom_card_template, null)
-
-        // Encuentra los elementos dentro de la "carta"
-        card_title = customCardView.findViewById(R.id.card_title_activity)
-        card_subtitle = customCardView.findViewById(R.id.card_subtitle_activity)
-        card_username = customCardView.findViewById(R.id.card_username_activity)
-        card_maps = customCardView.findViewById(R.id.card_maps_activity)
-
-        // Establece los textos personalizados
-        card_title.text = getString(R.string.home_ereaga_text)
-        card_subtitle.text = getString(R.string.home_getxo_text)
-        card_username.text = getString(R.string.home_username_text)
 
         // Agrega la vista de la "carta" personalizada a tu diseño principal
         val mainLayout = findViewById<LinearLayout>(R.id.mainLayout)
-        mainLayout.addView(customCardView)
+
+        for (i in 1..2) {
+            // Infla la vista que contiene la "carta"
+            val customCardView = layoutInflater.inflate(R.layout.custom_card_template, null)
+
+            // Encuentra los elementos dentro de la "carta"
+            card_title = customCardView.findViewById(R.id.card_title_activity)
+            card_subtitle = customCardView.findViewById(R.id.card_subtitle_activity)
+            card_username = customCardView.findViewById(R.id.card_username_activity)
+            card_maps = customCardView.findViewById(R.id.card_maps_activity)
+
+            // Establece los textos personalizados
+            card_title.text = getString(R.string.home_ereaga_text)
+            card_subtitle.text = getString(R.string.home_getxo_text)
+            card_username.text = getString(R.string.home_username_text)
+
+            mainLayout.addView(customCardView)
+        }
 
 
 
-
-
+        findViewById<AppCompatImageView>(R.id.profile_picture).setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/txurdinaga/rednit_app/views/ProfileActivity.kt
+++ app/src/main/java/com/txurdinaga/rednit_app/views/ProfileActivity.kt
@@ -1,8 +1,12 @@
 package com.txurdinaga.rednit_app.views
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.*
+import androidx.appcompat.widget.AppCompatImageView
 import com.txurdinaga.rednit_app.R
 
 class ProfileActivity : AppCompatActivity() {
@@ -12,5 +16,18 @@ class ProfileActivity : AppCompatActivity() {
 
         Log.i("project|main", "ProfileActivity has started!")
 
+
+        findViewById<Button>(R.id.logout_user_button).setOnClickListener {
+            /**
+             * @description: Clear stored credentials
+             */
+            val sharedPreferences = getSharedPreferences("userCredentials", MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putString("email", null)
+            editor.putString("password", null)
+            editor.apply()
+
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
